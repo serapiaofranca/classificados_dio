8a202399e62a5c384fcb1f63a8fd3b8063fbd8c8
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-29 21:07:14 -0300

    test model_user_validate_email implemented

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,9 +4,25 @@ class UserTest < ActiveSupport::TestCase
   
   test "nome é obrigatório" do
     user = User.new(name: nil)
-
     user.save
 
     assert user.errors[:name].any?
+  end
+
+  test "email é obrigatório e no formato válido" do
+    user = User.new(email: nil)
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: "invalido")
+    user.save
+
+    assert user.errors[:email].any?
+
+    user = User.new(email: "valido@exemplo.com")
+    user.save
+
+    refute user.errors[:email].any?
   end
 end
