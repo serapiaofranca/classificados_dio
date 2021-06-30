947ce3700c1097bf206e6eda047b25dd5e1fa604
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-29 22:48:24 -0300

    refactory routes resources, and message i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#show"
 
-  get "/users/new", to: "users#new"
-  post "/users", to: "users#create"
+  resources :users, only: [:new, :create]
 
-  get "/sessions/new", to: "sessions#new"
+  resources :sessions, only: [:new]
 end
