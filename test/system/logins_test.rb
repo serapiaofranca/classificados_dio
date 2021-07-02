78cb1e6e5ce1aeb3e9cb22ea7aab35628f166581
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-01 22:07:55 -0300

    test invalid_login implemented

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,17 +1,36 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
-  test "Login com credenciais validas" do
-    user = FactoryBot.create(:user, password: "password", password_confirmation: "password") 
 
+  def setup
+    @user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
+  end
+
+  test "Login com credenciais validas" do 
     visit root_path
     click_link "Entrar"
 
-    fill_in "E-mail",	with: user.email
+    fill_in "E-mail",	with: @user.email
     fill_in "Senha", with: "password"
 
     click_button "Entrar"
 
-    assert_text("Bem vindo #{user.name}")
+    assert_text("Bem vindo #{@user.name}")
   end
+
+  test "Login com credenciais invalidas" do
+    
+    visit root_path
+    click_link "Entrar"
+
+    fill_in "E-mail",	with: @user.email
+    fill_in "Senha", with: "invalidpassword"
+
+    click_button "Entrar"
+
+    assert_equal new_session_path, current_path
+    assert_text "E-mail ou senha invalidos"
+
+  end
+ 
 end
